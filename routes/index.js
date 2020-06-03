9a692f03034ea9f06a104b53e451e224a0b43a70
Avinash <abhishekkumar843134@gmail.com>
2020-06-04 00:48:09 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const bcrypt = require("bcryptjs");
 const { ensureAuthenticated, ensureGuest } = require("../auth/auth");
 
 const User = require("../models/user");
